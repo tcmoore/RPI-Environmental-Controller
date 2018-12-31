1ab5c4ef0f173f0a770a992c5bea76967c280378
tcmoore <tcmoore@illinois.edu>
2018-12-30 23:13:26 -0600

    RPI GPIO LED Test

---
+++ rpi_gpio_test.py
@@ -0,0 +1,9 @@
+# GPIO LED EXAMPLE TO TEST CONTROLLING RPI GPIO
+# T.MOORE
+# 12.29.18
+
+import RPi.GPIO as GPIO     ## Import GPIO library
+
+GPIO.setmode(GPIO.BOARD)    ## Use board pin numbering
+GPIO.setup(36, GPIO.OUT)    ## Setup GPIO pin 36 to OUT
+GPIO.output(36, False)       ## Turn on GPIO pin 36- 
