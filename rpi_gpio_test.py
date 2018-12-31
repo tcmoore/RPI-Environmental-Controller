8148f92ac0e3be05a5887d9234615b1e063a0759
tcmoore <tcmoore@illinois.edu>
2018-12-30 23:14:54 -0600

    Turn on/off GPIO pin 36 to turn on/off led

--- rpi_gpio_test.py
+++ rpi_gpio_test.py
@@ -7,3 +7,4 @@ import RPi.GPIO as GPIO     ## Import GPIO library
 GPIO.setmode(GPIO.BOARD)    ## Use board pin numbering
 GPIO.setup(36, GPIO.OUT)    ## Setup GPIO pin 36 to OUT
 GPIO.output(36, False)       ## Turn on GPIO pin 36- 
+ 
