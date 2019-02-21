752f012223e1b13776197813f36d7795b33fe447
Todd Moore <tcmoore@illinois.edu>
2019-02-21 15:03:27 -0600

    Added code to read the Grove soil moisture sensor

---
+++ code/grovepi_plus_get_soil_moisture.py
@@ -0,0 +1,29 @@
+# Get soil moisture from Grove moisture sensor connected to the GrovePi+ Hat
+# http://wiki.seeedstudio.com/Grove-Moisture_Sensor/
+
+# Sensor is connected to A2 of the GrovePi+ Hat
+
+#   Here are suggested sensor values:
+#       Min  Typ  Max  Condition
+#       0    0    0    sensor in open air
+#       0    20   300  sensor in dry soil
+#       300  580  700  sensor in humid soil
+#       700  940  950  sensor in water
+
+
+import time
+import grovepi
+
+# Connect the Grove Moisture Sensor to analog port A2
+# SIG,NC,VCC,GND
+sensor = 2
+
+while True:
+    try:
+        print(grovepi.analogRead(sensor))
+        time.sleep(.5)
+
+    except KeyboardInterrupt:
+        break
+    except IOError:
+        print ("Error")
