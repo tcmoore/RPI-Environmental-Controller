de675245406bb532ee8070947994cadd755173bc
Todd Moore <tcmoore@illinois.edu>
2019-02-22 11:09:09 -0600

    Update grovepi_plus_temp_alarm.py

--- code/grovepi_plus_temp_alarm.py
+++ code/grovepi_plus_temp_alarm.py
@@ -6,7 +6,7 @@
 # Connect Temperature Alarm LED to digital port D7
 temp_alarm_led = 6
 
-pinMode(led,"OUTPUT")
+pinMode(temp_alarm_led,"OUTPUT")
 time.sleep(1)
 
 print ("This example will blink an LED connected to the GrovePi+ on the port labeled D6. If you're having trouble seeing the LED blink, be sure to check the LED connection and the port number. You may also try reversing the direction of the LED on the sensor.")
