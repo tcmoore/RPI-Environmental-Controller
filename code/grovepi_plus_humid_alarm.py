dcf0d54186738cb594c6459a5a2463a4492402a0
Todd Moore <tcmoore@illinois.edu>
2019-02-22 11:08:40 -0600

    Update grovepi_plus_humid_alarm.py

--- code/grovepi_plus_humid_alarm.py
+++ code/grovepi_plus_humid_alarm.py
@@ -6,7 +6,7 @@
 # Connect Temperature Alarm LED to digital port D7
 humid_alarm_led = 7
 
-pinMode(led,"OUTPUT")
+pinMode(humid_alarm_led,"OUTPUT")
 time.sleep(1)
 
 print ("This example will blink an LED connected to the GrovePi+ on the port labeled D7. If you're having trouble seeing the LED blink, be sure to check the LED connection and the port number. You may also try reversing the direction of the LED on the sensor.")
