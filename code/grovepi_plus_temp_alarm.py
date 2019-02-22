7e30f644ef0bba182f9385b904934f2855bf29c1
Todd Moore <tcmoore@illinois.edu>
2019-02-22 11:06:38 -0600

    changed the which port to connect the led

--- code/grovepi_plus_temp_alarm.py
+++ code/grovepi_plus_temp_alarm.py
@@ -1,17 +1,17 @@
-# Turn on/off Temperature Alarm LED connected to GrovePi+ port D7
+# Turn on/off Temperature Alarm LED connected to GrovePi+ port D6
 # I'll be using my own LEDs instead of the Grove LEDs.  However, I'm using the code, grove_led_blink.py,
 # to drive the led from here:
 # http://wiki.seeedstudio.com/Grove-Red_LED/
 
 # Connect Temperature Alarm LED to digital port D7
-temp_alarm_led = 7
+temp_alarm_led = 6
 
 pinMode(led,"OUTPUT")
 time.sleep(1)
 
-print ("This example will blink an LED connected to the GrovePi+ on the port labeled D7. If you're having trouble seeing the LED blink, be sure to check the LED connection and the port number. You may also try reversing the direction of the LED on the sensor.")
+print ("This example will blink an LED connected to the GrovePi+ on the port labeled D6. If you're having trouble seeing the LED blink, be sure to check the LED connection and the port number. You may also try reversing the direction of the LED on the sensor.")
 print (" ")
-print ("Connect the LED to the port labele D4!" )
+print ("Connect the LED to the port labele D6!" )
 
 while True:
     try:
